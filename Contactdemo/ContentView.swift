02f01a52bbb529daa3c8a9f9b4bd3832afde4e66
Bharat <bharat@quintessential.design>
2020-04-13 09:06:42 +0530

    modified images and added gmail

--- Contactdemo/ContentView.swift
+++ Contactdemo/ContentView.swift
@@ -15,27 +15,27 @@ struct ContentView: View {
     @State var showContactPicker = false
     
     var body: some View {
+       
         ZStack{
         VStack{
             Spacer(minLength: 500)
             List(){
-            
             if self.pickedContact != nil {
 
                 HStack {
+                    
                     if ((self.pickedContact?.imageData) != nil) {
                         Image(uiImage: UIImage(data: (pickedContact?.imageData)!)!)
                             .resizable()
-                            .frame(width: 100, height: 100)
+                            .frame(width: 80, height: 80)
                             .clipShape(Circle())
                             .overlay(Circle().stroke(Color.white,lineWidth:2))
                             .shadow(radius:1)
                     }else{
                         Text("\(pickedContact!.givenName.first(char: 1))\(pickedContact!.familyName.first(char: 1))")
-                            .font(.headline)
-                            .frame(width: 100, height: 100)
+                            .font(.title)
+                            .frame(width: 80, height: 80)
                             .foregroundColor(.white)
-                            .frame(width:40,height: 40)
                             .background(Color.blue)
                             .clipShape(Circle())
                             .overlay(Circle().stroke(Color.white,lineWidth:0.5))
@@ -47,7 +47,7 @@ struct ContentView: View {
                             .font(.body)
                         Text("\(self.pickedContact!.phoneNumbers.first!.value.stringValue)")
                             .font(.body)
-                        Text("\((self.pickedContact?.emailAddresses.first?.value) ?? "")")
+                        Text("\((self.pickedContact?.emailAddresses.first?.value) ?? "No Email")")
                             .font(.body)
                     }
                 }
@@ -82,25 +82,7 @@ struct ContentView: View {
             }
         }
     }
-    
-//    func getInitail() -> String {
-//        if (self.pickedContact?.familyName) != nil{
-//            return pickedContact?.familyName.utf8.prefix(2)
-//        }
-//
-//        return "tt"
-//    }
-    
-    
-    
-    
-    func getImage() -> UIImage {
-        if ((self.pickedContact?.imageData) != nil) {
-            return UIImage(data:(pickedContact?.imageData)!)!
-          }
-        return UIImage(data:(pickedContact?.imageData)!)!
-      }
-    
+  
     init() {
                        UITableView.appearance().backgroundColor = .clear
                        UITableViewCell.appearance().backgroundColor = .clear
